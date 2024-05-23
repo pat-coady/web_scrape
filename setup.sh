82a051cf6dcecbe96b1da1e883612fa6379ca018
pat-coady <pcoady00@gmail.com>
2024-05-23 15:44:04 +0000

    Initial commit.

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo apt update
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo apt install python3 python3-pip python3-notebook
+sudo apt install python3-selenium 
