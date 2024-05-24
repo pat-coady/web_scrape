2b553a762e485508896a90453e76a67c55c5647a
pat-coady <pcoady00@gmail.com>
2024-05-24 21:26:29 +0000

    Add JUPYTER ENV variable to .bashrc

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 sudo apt update
+# install chromumium as browser that selenium driver will connect to
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install -y ./google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
+
 sudo apt install -y python3 python3-pip python3-notebook
 sudo apt install -y python3-selenium
 export JUPYTER_CONFIG_DIR="."
-echo "export $ENV_VAR=$VALUE" >> ~/.bashrc
-
