d3a93003895f1e9bfe0d1a7c63821b1017296ce8
pat-coady <pcoady00@gmail.com>
2024-05-24 12:33:17 +0000

    Non-interactive apt install

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 sudo apt update
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
-sudo apt install python3 python3-pip python3-notebook
-sudo apt install python3-selenium 
+sudo DEBIAN_FRONTEND=noninteractive
+apt install -y ./google-chrome-stable_current_amd64.deb
+sudo apt install -y python3 python3-pip python3-notebook
+sudo apt install -y python3-selenium 
