7e695c97eac2ec13edc60785aeb5266838c04af1
pat-coady <pcoady00@gmail.com>
2024-05-24 21:00:53 +0000

    cleanup notebook and startup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 sudo apt update
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install -y ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
 sudo apt install -y python3 python3-pip python3-notebook
-sudo apt install -y python3-selenium 
+sudo apt install -y python3-selenium
+export JUPYTER_CONFIG_DIR="."
+echo "export $ENV_VAR=$VALUE" >> ~/.bashrc
+
