14224dcd6bbab523160a758a9824694bc1ed7403
pat-coady <pcoady00@gmail.com>
2024-05-24 14:03:35 +0000

    remove DEBIAN_FRONTEND env var

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 sudo apt update
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo DEBIAN_FRONTEND=noninteractive
 apt install -y ./google-chrome-stable_current_amd64.deb
 sudo apt install -y python3 python3-pip python3-notebook
 sudo apt install -y python3-selenium 
